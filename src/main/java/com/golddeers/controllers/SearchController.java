107eb4c6dc862b24c51a80cba5078ad2394e9184
Necati Berk Ozgur <necatiberkozgur@gmail.com>
2020-05-19 17:05:00 +0300

    started implementation for search feature

---
+++ src/main/java/com/golddeers/controllers/SearchController.java
@@ -0,0 +1,24 @@
+package com.golddeers.controllers;
+
+import java.util.List;
+
+import com.golddeers.model.Book;
+
+public class SearchController {
+
+	public List<Book> getBooks(String searchString) {
+
+		return null;
+	}
+
+	public List<Book> filter(List<Book> books, String... filters) {
+
+		return null;
+	}
+
+	public List<Book> sort(List<Book> books, String sortOption) {
+
+		return null;
+	}
+
+}
---
+++ src/main/java/com/golddeers/model/SearchModel.java
@@ -0,0 +1,11 @@
+package com.golddeers.model;
+
+import java.util.List;
+
+public class SearchModel {
+
+	public List<Book> getBooks(String searchString, Enum<?>... options) {
+
+		return null;
+	}
+}
