46a106e6e8302fff3c7798729a1e5aa4ba0ba857
Necati Berk Ozgur <necatiberkozgur@gmail.com>
2020-06-13 22:58:28 +0300

    Search/Filter options improved, feedback feature implemented

--- src/main/java/com/golddeers/controllers/FeedbackController.java
+++ src/main/java/com/golddeers/controllers/FeedbackController.java
@@ -1,18 +1,38 @@
 package com.golddeers.controllers;
 
+import javax.mail.MessagingException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.golddeers.commands.FeedbackForm;
+import com.golddeers.email.SendEmail;
 
 @Controller
 public class FeedbackController {
 
-	@RequestMapping({ "/contact" })
-	public String leaveFeedback(Model model) {
+	@RequestMapping(value = "/contact")
+	public String sendFeedbackForm(Model model) {
 
-		model.addAttribute("contactForm", new FeedbackForm());
+		return "winter/contact";
+	}
+
+	@RequestMapping(value = "/contact", method = RequestMethod.POST)
+	public String leaveFeedback(@RequestParam(name = "message") String message,
+			@RequestParam(name = "email") String email, @RequestParam(name = "subject") String subject,
+			@RequestParam(name = "name") String name, Model model) {
+
+		try {
+
+			SendEmail.sendEmail("necatiberkozgur@gmail.com", name + " said: \n" + message, subject);
+
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("isSent", true);
 		return "winter/contact";
 	}
 }
--- src/main/java/com/golddeers/controllers/SearchController.java
+++ src/main/java/com/golddeers/controllers/SearchController.java
@@ -1,24 +1,38 @@
 package com.golddeers.controllers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.golddeers.model.Book;
+import com.golddeers.model.Category;
 import com.golddeers.services.BookService;
+import com.golddeers.services.CategoryService;
 
 @Controller
 public class SearchController {
 
 	private BookService bookService;
+	private CategoryService categoryService;
 
 	private static Set<Book> foundStaticBooks;
+
+	@Autowired
+	public void setCategoryService(CategoryService categoryService) {
+
+		this.categoryService = categoryService;
+	}
 
 	@Autowired
 	public void setBookService(BookService bookService) {
@@ -28,11 +42,18 @@ public class SearchController {
 
 	@RequestMapping(value = "/filter", method = RequestMethod.POST)
 	public String filterByAuthor(@RequestParam(name = "authors_dropdown") String author,
-			@RequestParam(name = "genres_dropdown") String genre, Model model) {
+			@RequestParam(name = "genres_dropdown") String genre,
+			@RequestParam(name = "sort_dropdown") String sortOption, Model model) {
 
 		Set<Book> retBooks = new HashSet<Book>();
-		Set<String> retGenre = new HashSet<String>();
 		Set<String> retAuthors = new HashSet<String>();
+
+		List<Book> retList = new ArrayList<Book>();
+
+		List<Category> genres = categoryService.listAll();
+		Set<String> retGenre = new HashSet<String>();
+		genres.forEach((category) -> retGenre.add(category.getName()));
+
 		System.out.println(genre);
 		System.out.println(author);
 
@@ -43,11 +64,58 @@ public class SearchController {
 
 				retBooks.add(book);
 			}
-			retGenre.add(book.getGenre());
+
 			retAuthors.add(book.getAuthor());
 		});
 
-		model.addAttribute("books", retBooks);
+		retList = new ArrayList<Book>(retBooks);
+
+		if (sortOption.equals("none") == false) {
+
+			if (sortOption.equals("pri_asc") == true) {
+
+				Collections.sort(retList, new Comparator<Book>() {
+					@Override
+					public int compare(Book b2, Book b1) {
+
+						return b2.getPrice().compareTo(b1.getPrice());
+					}
+				});
+
+			} else if (sortOption.equals("pri_desc") == true) {
+
+				Collections.sort(retList, new Comparator<Book>() {
+					@Override
+					public int compare(Book b2, Book b1) {
+
+						return b1.getPrice().compareTo(b2.getPrice());
+					}
+				});
+
+			} else if (sortOption.equals("name_asc") == true) {
+
+				Collections.sort(retList, new Comparator<Book>() {
+					@Override
+					public int compare(Book b2, Book b1) {
+
+						return b2.getDescription().compareTo(b1.getDescription());
+					}
+				});
+
+			} else if (sortOption.equals("name_desc") == true) {
+
+				Collections.sort(retList, new Comparator<Book>() {
+					@Override
+					public int compare(Book b2, Book b1) {
+
+						return b1.getDescription().compareTo(b2.getDescription());
+					}
+				});
+
+			}
+		}
+
+		model.addAttribute("books", retList);
 		model.addAttribute("authors", retAuthors);
 		model.addAttribute("genres", retGenre);
 
@@ -59,7 +127,9 @@ public class SearchController {
 
 		Set<Book> foundBooks = new HashSet<>();
 		Set<String> foundAuthors = new HashSet<>();
-		Set<String> foundGenres = new HashSet<>();
+		List<Category> genres = categoryService.listAll();
+		Set<String> retGenre = new HashSet<String>();
+		genres.forEach((category) -> retGenre.add(category.getName()));
 
 		foundBooks.addAll(bookService.findByDescriptionContaining(myid));
 		foundBooks.addAll(bookService.findByAuthorIgnoreCaseContaining(myid));
@@ -70,12 +140,36 @@ public class SearchController {
 
 			foundAuthors.add(book.getAuthor());
 
-			foundGenres.add(book.getGenre());
-			System.out.println(book.getDescription());
 		});
 
 		model.addAttribute("books", foundBooks);
-		model.addAttribute("genres", foundGenres);
+		model.addAttribute("genres", retGenre);
+		model.addAttribute("authors", foundAuthors);
+
+		return "/winter/list";
+	}
+
+	@RequestMapping(value = "/search/{category}")
+	public String searchByCategory(@PathVariable(name = "category") String category, Model model) {
+
+		Set<Book> foundBooks = new HashSet<>();
+		Set<String> foundAuthors = new HashSet<>();
+		List<Category> genres = categoryService.listAll();
+		Set<String> retGenre = new HashSet<String>();
+
+		genres.forEach((cat) -> retGenre.add(cat.getName()));
+
+		foundBooks.addAll(bookService.findByGenreIgnoreCaseContaining(category));
+
+		foundStaticBooks = foundBooks;
+		foundBooks.forEach((book) -> {
+
+			foundAuthors.add(book.getAuthor());
+
+		});
+
+		model.addAttribute("books", foundBooks);
+		model.addAttribute("genres", retGenre);
 		model.addAttribute("authors", foundAuthors);
 
 		return "/winter/list";
